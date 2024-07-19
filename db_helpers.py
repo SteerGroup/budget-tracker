8402772db1eedb9520b49980ef838654015aee5f
Andrew Desautels <andrew.desautels@steergroup.com>
2024-07-19 14:52:46 -0600

    Begin integration of db_helpers and Project class

--- db_helpers.py
+++ db_helpers.py
@@ -6,6 +6,10 @@ BUDGET_DB_FILE = "budget.db"
 
 def create_db():
     Path(BUDGET_DB_FILE).touch(exist_ok=True)
+    db_obj = get_db_objects()
+    create_projects_table(db_obj["cur"])
+    create_budget_details_table(db_obj["cur"])
+    db_obj["con"].close()
 
 
 def get_db_objects():
@@ -13,11 +17,10 @@ def get_db_objects():
     return {"con": con, "cur": con.cursor()}
 
 
-def create_projects_table():
-    db_obj = get_db_objects()
-    db_obj["cur"].execute(
+def create_projects_table(db_cur):
+    db_cur.execute(
         """
-        CREATE TABLE projects(
+        CREATE TABLE IF NOT EXISTS projects(
             name TEXT,
             code TEXT PRIMARY KEY,
             open INTEGER
@@ -26,5 +29,26 @@ def create_projects_table():
     )
 
 
-def create_budget_details_table():
-    pass
+def create_budget_details_table(db_cur):
+    db_cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS budget_details(
+            project_name TEXT,
+            period INTEGER,
+            task INTEGER,
+            staff TEXT,
+            planned_budget REAL,
+            actual_budget REAL
+        )
+        """
+    )
+
+
+def add_project(project):
+    db_obj = get_db_objects()
+    db_obj["cur"].execute(
+        "INSERT INTO projects VALUES(?, ?, ?);",
+        (project.name, project.code, project.open),
+    )
+    db_obj["con"].commit()
+    db_obj["con"].close()
--- project.py
+++ project.py
@@ -1,54 +1,57 @@
 import csv
 import yaml
+import db_helpers
 
 
 class Project:
     def __init__(self, yaml_path, budget_csv_path=None):
+        db_helpers.create_db()
         self.yaml_path = yaml_path
         self.parse_yaml()
         if budget_csv_path:
             self.parse_budget_csv()
 
-    def parse_yaml(self):
+    def parse_yaml(self):  # TODO: reimplement commented properties as DB calcs
         with open(self.yaml_path, "r") as file:
             project_yaml = yaml.safe_load(file)
             self.name = project_yaml["name"]
             self.code = project_yaml["code"]
             self.open = project_yaml["open"]
-            self.total_budget = project_yaml["total_budget"]
-            self.amount_spent = project_yaml["amount_spent"]
-            self.duration = project_yaml["duration"]
-            self.staff_count = project_yaml["staff_count"]
-            self.staff_list = project_yaml["staff_list"]
+            db_helpers.add_project(self)
+            # self.total_budget = project_yaml["total_budget"]
+            # self.amount_spent = project_yaml["amount_spent"]
+            # self.duration = project_yaml["duration"]
+            # self.staff_count = project_yaml["staff_count"]
+            # self.staff_list = project_yaml["staff_list"]
 
-    def write_yaml(self):
-        project_dict = {
-            "name": self.name,
-            "code": self.code,
-            "open": self.open,
-            "total_budget": self.total_budget,
-            "amount_spent": self.amount_spent,
-            "duration": self.duration,
-            "staff_count": self.staff_count,
-            "staff_list": self.staff_list,
-        }
-        with open(self.yaml_path, "w") as file:
-            yaml.dump(project_dict, file)
+    # def write_yaml(self):
+    #     project_dict = {
+    #         "name": self.name,
+    #         "code": self.code,
+    #         "open": self.open,
+    #         "total_budget": self.total_budget,
+    #         "amount_spent": self.amount_spent,
+    #         "duration": self.duration,
+    #         "staff_count": self.staff_count,
+    #         "staff_list": self.staff_list,
+    #     }
+    #     with open(self.yaml_path, "w") as file:
+    #         yaml.dump(project_dict, file)
 
     def parse_budget_csv(budget_csv_path):
         pass
 
-    def get_avg_burn_rate(self):
+    def get_avg_burn_rate(self):  # TODO: Update
         return self.total_budget / self.duration
 
-    def get_pct_budget_used(self):
+    def get_pct_budget_used(self):  # TODO: Update
         if self.total_budget == 0:
             raise ValueError("Budget cannot be zero.")
         if not isinstance(self.total_budget, int):
             raise ValueError("Budget must be numeric.")
         return self.amount_spent / self.total_budget
 
-    def get_remaining_budget(self, percent=False):
+    def get_remaining_budget(self, percent=False):  # TODO: Update
         amt_remaining = self.total_budget - self.amount_spent
         if percent:
             return amt_remaining / self.total_budget
